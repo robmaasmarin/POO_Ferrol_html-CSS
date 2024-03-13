02764be8614907b8c2302580061db97c1609bdd0
robmaasmarin <rabelleirap@gmail.com>
2024-03-13 13:23:54 +0100

    wednesday

---
+++ JavaScript/Scripts/funciones.js
@@ -0,0 +1,11 @@
+function saluda() {
+    document.write("Hello world")
+}
+
+
+function suma() {
+    var x,y;
+ x = prompt("Ingrese numero uno: ");
+ y = prompt("Ingrese numero dos: ");
+ document.write("El resultado es: " , (parseInt(x)+parseInt(y)))
+}
