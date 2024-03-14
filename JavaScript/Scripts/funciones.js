3263a68f22a1b76cf0abdff75ef2a7d9f1b4e231
robmaasmarin <rabelleirap@gmail.com>
2024-03-14 13:28:25 +0100

    thursday 14

--- JavaScript/Scripts/funciones.js
+++ JavaScript/Scripts/funciones.js
@@ -7,5 +7,9 @@ function suma() {
     var x,y;
  x = prompt("Ingrese numero uno: ");
  y = prompt("Ingrese numero dos: ");
+ 
  document.write("El resultado es: " , (parseInt(x)+parseInt(y)))
 }
+
+
+
