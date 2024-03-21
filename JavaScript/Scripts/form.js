57b3cf40f2a527aebb9f705a2dc17e030169c1dc
robmaasmarin <rabelleirap@gmail.com>
2024-03-21 13:25:46 +0100

    thurdsday contact form

---
+++ JavaScript/Scripts/form.js
@@ -0,0 +1,10 @@
+
+
+function updateDiv() {
+    document.getElementById("gracias").innerHTML = "Formulario enviado correctamente. Gracias por tu mensaje.";
+    document.getElementById("print").style.backgroundColor = "red";
+    document.getElementById("contactform").reset();
+    
+
+    
+}
---
+++ JavaScript/Scripts/printform.js
@@ -0,0 +1,9 @@
+function printDiv() { 
+    var divContents = document.getElementById("contactform").innerHTML; 
+    var a = window.open('', '', 'height=500, width=500'); 
+    
+    a.document.write(divContents); 
+    
+    a.document.close(); 
+    a.print(); 
+} 
