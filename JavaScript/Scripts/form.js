cbe5b8677304775e2b4d145447c132ec23aeda16
robmaasmarin <rabelleirap@gmail.com>
2024-03-22 13:24:20 +0100

    friday22

--- JavaScript/Scripts/form.js
+++ JavaScript/Scripts/form.js
@@ -2,9 +2,34 @@
 
 function updateDiv() {
     document.getElementById("gracias").innerHTML = "Formulario enviado correctamente. Gracias por tu mensaje.";
-    document.getElementById("print").style.backgroundColor = "red";
+    
+    let text = "¿Quieres imprimir tu mensaje?";
+    if (confirm(text) == true) {
+        printDiv();
+      } else {
+        text = "You canceled!";
+      }
+
+
+
+
+
+
     document.getElementById("contactform").reset();
     
 
     
 }
+
+
+
+
+function printDiv() { 
+    var divContents = document.getElementById("contactform").innerHTML; 
+    var a = window.open('', '', 'height=500, width=500'); 
+    
+    a.document.write(divContents); 
+    
+    a.document.close(); 
+    a.print(); 
+} 
---
+++ JavaScript/Scripts/signin.js
@@ -0,0 +1,58 @@
+function myFunction() {
+    const user1 = "bob";
+    const pass1 = "123";
+    const user2 = "rob";
+    const pass2 = "456";
+    const user3 = "kob";
+    const pass3 = "789";
+    var userlogin = document.getElementById("username").value;
+    var passlogin = document.getElementById("pwd").value;
+
+    
+
+
+     switch(userlogin) {
+        case user1:
+            if (passlogin == pass1) {
+                /* CARGAR OTRA PÁGINA CUANDO EL LOGIN ES SATISFACTORIO */
+                location.href = "welcome.html";
+
+
+            }
+            else {
+                document.getElementById("invalid").style.color = "red"
+                document.getElementById("invalid").style.fontWeight = "bold";
+                document.getElementById("invalid").innerHTML = "Wrong username or password. Please, try again."
+            }
+          // code block
+          break;
+        case user2: 
+            if(passlogin == pass2) {
+            document.getElementById("logininfo").innerHTML = "SUCCESS"
+            }
+            else {
+                document.getElementById("invalid").style.color = "red"
+                document.getElementById("invalid").style.fontWeight = "bold";
+                document.getElementById("invalid").innerHTML = "Wrong username or password. Please, try again."
+            }
+          // code block
+          break;
+        case user3:
+            if (passlogin == pass3) {document.getElementById("logininfo").innerHTML = "SUCCESS"
+            }
+            else {
+                document.getElementById("invalid").style.color = "red"
+                document.getElementById("invalid").style.fontWeight = "bold";
+                document.getElementById("invalid").innerHTML = "Wrong username or password. Please, try again."
+            }
+      // code block
+      break;
+
+                
+        default:
+          // code block
+                document.getElementById("invalid").style.color = "red"
+                document.getElementById("invalid").style.fontWeight = "bold";
+                document.getElementById("invalid").innerHTML = "Wrong username or password. Please, try again."
+      }
+  }
